53059facbbc42878b77dc3f1220c4fde7d68d988
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-11-23 18:43:05 +0530

    keep track of score of both players

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -20,7 +22,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,10 +34,14 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    # Detect if ball passed the paddle
-    if ball.xcor() > 360:
+    # Detect R paddle misses
+    if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
-    if ball.xcor() < -360:
+    # Detect L paddle misses
+    if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
+
 screen.exitonclick()
